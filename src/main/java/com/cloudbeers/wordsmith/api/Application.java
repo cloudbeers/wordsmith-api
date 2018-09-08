accc9607a4171aee52de5099bdc7dd0984ad23c5
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-09-08 19:26:27 +0200

    add a TODO

--- src/main/java/com/cloudbeers/wordsmith/api/Application.java
+++ src/main/java/com/cloudbeers/wordsmith/api/Application.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * TODO a todo message to demo Jenkins source code analysis capabilities
+ */
 @SpringBootApplication
 public class Application {
 
